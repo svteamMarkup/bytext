e56ae076d403c4f21450da1df24da846746daeee
extiser <1sappho@gmail.com>
2017-04-24 15:24:50 +0500

    fix

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -10252,11 +10252,11 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
+
 /*$(document).on('click','.textarea-item__title',function() {
 	$(this).children('.textarea-item__towrite').slideToggle(300);
 	return false;
 });*/
-
 
 
 
